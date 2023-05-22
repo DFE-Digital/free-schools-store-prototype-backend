c154320352fe670ace834e084ef43d8cdcfa03cd
Jack Marshall <jackmarshall@SLWT633532.AD.HQ.DEPT>
2023-05-22 13:08:54 +0100

    Finished delete method

---
+++ Dfe.OpenFreeSchools/Pages/Project/Delete.cshtml.cs
@@ -0,0 +1,112 @@
+
+using Dfe.OpenFreeSchools.Services.Project;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Dfe.OpenFreeSchools.Pages.Project
+{
+    public class CreateProjectModel : PageModel
+    {
+        [BindProperty]
+        [MaxLength(10)]
+        public string ProjectID { get; set; }
+
+        [BindProperty]
+        [MaxLength(20)]
+        public string SchoolName { get; set; }
+
+        [BindProperty]
+        [MaxLength(10)]
+        public string ApplicationNumber { get; set; }
+
+        [BindProperty]
+        [MaxLength(10)]
+        public string ApplicationWave { get; set; }
+        public ICreateProjectService _createProjectService { get; }
+    //    public ILogger<CreateProjectModel> _logger { get; }
+
+        public CreateProjectModel(
+            ICreateProjectService createProjectService
+         //   ILogger<CreateProjectModel> logger
+            )
+        {
+            _createProjectService = createProjectService;
+            //_logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //_logger.LogMethodEntered();
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return Page();
+                }
+
+                var caseUrn = await _createProjectService.CreateProject(ProjectID, SchoolName, ApplicationNumber, ApplicationWave, "Sukhy");
+
+                return Redirect($"/");
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError(ex, "error");
+
+                // TempData["Error.Message"] = ErrorOnPostPage;
+            }
+
+            return Page();
+        }
+    }
+    public class DeleteProjectModel : PageModel
+    {
+        [BindProperty]
+        [MaxLength(10)]
+        public string ProjectID { get; set; }
+
+        public IDeleteProjectService _deleteProjectService { get; }
+        //    public ILogger<CreateProjectModel> _logger { get; }
+
+        public DeleteProjectModel(
+            IDeleteProjectService deleteProjectService
+            //   ILogger<CreateProjectModel> logger
+            )
+        {
+            _deleteProjectService = deleteProjectService;
+            //_logger = logger;
+        }
+
+        public async Task<IActionResult> OnDeleteAsync()
+        {
+            //_logger.LogMethodEntered();
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return Page();
+                }
+
+                var caseUrn = await _deleteProjectService.DeleteProject(ProjectID);
+
+                return Redirect($"/");
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError(ex, "error");
+
+                // TempData["Error.Message"] = ErrorOnPostPage;
+            }
+
+            return Page();
+        }
+    }
+}
